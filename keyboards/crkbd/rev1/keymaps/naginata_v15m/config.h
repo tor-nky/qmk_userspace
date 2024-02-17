71e2c85b45e87f28f27dd853ab55810522612df9
Satoru NAKAYA <HKR7Fm8P7U@gmail.com>
2024-02-17 09:30:51 +0900

    config.hの役割分担

--- keyboards/crkbd/rev1/keymaps/naginata_v15m/config.h
+++ keyboards/crkbd/rev1/keymaps/naginata_v15m/config.h
@@ -36,8 +36,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // iOS device need lessthan 100
 // #define USB_MAX_POWER_CONSUMPTION 100
 
-#define UNICODE_TYPE_DELAY 0
-
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLIGHT_ANIMATIONS
     #undef RGBLIGHT_EFFECT_BREATHING
@@ -124,8 +122,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 #define NG_USE_KAWASEMI // Mac汎用 → かわせみ専用
-
-#ifdef NG_USE_KAWASEMI
-// Keycodes used for starting Unicode input on different platforms
-#   define UNICODE_KEY_MAC KC_NO   // かわせみ3用
-#endif
--- keyboards/crkbd_rp/keymaps/naginata_v15m/config.h
+++ keyboards/crkbd_rp/keymaps/naginata_v15m/config.h
@@ -36,8 +36,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // iOS device need lessthan 100
 // #define USB_MAX_POWER_CONSUMPTION 100
 
-#define UNICODE_TYPE_DELAY 0
-
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
     #define RGBLIGHT_EFFECT_RAINBOW_MOOD
@@ -122,11 +120,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 #define NG_USE_KAWASEMI // Mac汎用 → かわせみ専用
-
-#ifdef NG_USE_KAWASEMI
-// Keycodes used for starting Unicode input on different platforms
-#   define UNICODE_KEY_MAC KC_NO   // かわせみ3用
-#endif
 
 // Forces the keyboard to wait for a USB connection to be established before it starts up
 #define WAIT_FOR_USB
