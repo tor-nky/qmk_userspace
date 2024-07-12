485713d0ae4f4067549817699c90e677d3af968e
Satoru NAKAYA <HKR7Fm8P7U@gmail.com>
2024-07-12 17:41:55 +0900

    キーマップの微調整

--- keyboards/keyboard_quantizer/rev2/keymaps/naginata_v15m/keymap.c
+++ keyboards/keyboard_quantizer/rev2/keymaps/naginata_v15m/keymap.c
@@ -62,11 +62,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] =  LAYOUT(
          KC_ESC,   KC_F1,  KC_F2,  KC_F3,  KC_F4,    KC_F5,  KC_F6,  KC_F7,  KC_F8,    KC_F9, KC_F10, KC_F11, KC_F12,  KC_PSCR,KC_SCRL,KC_PAUS,
-        JP_ZKHK,    JP_1,   JP_2,   JP_3,   JP_4,   JP_5,   JP_6,   JP_7,   JP_8,   JP_9,   JP_0,JP_MINS,JP_CIRC, JP_YEN,KC_BSPC,   KC_INS,KC_HOME,KC_PGUP,   KC_NUM,KC_PSLS,KC_PAST,KC_PMNS,
+        JP_ZKHK,    JP_1,   JP_2,   JP_3,   JP_4,   JP_5,   JP_6,   JP_7,   JP_8,   JP_9,   JP_0,JP_MINS,JP_CIRC, JP_YEN,KC_BSPC,   KC_INS,KC_HOME,KC_PGUP,  JP_COLN,KC_PSLS,KC_PAST,KC_PMNS,
          KC_TAB,     JP_Q,   JP_W,   JP_E,   JP_R,   JP_T,   JP_Y,   JP_U,   JP_I,   JP_O,   JP_P,  JP_AT,JP_LBRC,        KC_ENT,   KC_DEL, KC_END,KC_PGDN,    KC_P7,  KC_P8,  KC_P9,KC_PPLS,
-        JP_CAPS,      JP_A,   JP_S,   JP_D,   JP_F,   JP_G,   JP_H,   JP_J,   JP_K,   JP_L,JP_SCLN,JP_COLN,JP_RBRC,                                            KC_P4,  KC_P5,  KC_P6,
+       S(JP_CAPS),    JP_A,   JP_S,   JP_D,   JP_F,   JP_G,   JP_H,   JP_J,   JP_K,   JP_L,JP_SCLN,JP_COLN,JP_RBRC,                                            KC_P4,  KC_P5,  KC_P6,
         KC_LSFT,       JP_Z,   JP_X,   JP_C,   JP_V,   JP_B,   JP_N,   JP_M,JP_COMM, JP_DOT,JP_SLSH,JP_BSLS,             KC_RSFT,            KC_UP,            KC_P1,  KC_P2,  KC_P3,KC_PENT,
-        KC_LCTL,KC_LGUI,   KC_LALT,     LOW_BS,          KC_SPC,       RAI_ENT,   JP_HENK,   KC_RALT,  KC_RGUI, KC_APP,  KC_RCTL,  KC_LEFT,KC_DOWN,KC_RGHT,    KC_P0,        KC_PDOT
+        KC_LCTL,KC_LGUI,   KC_LALT,     LOW_BS,        S(KC_SPC),       RAI_ENT,    KC_SPC,   KC_RALT,  KC_RGUI, KC_APP,  KC_RCTL,  KC_LEFT,KC_DOWN,KC_RGHT,    KC_P0,        KC_PDOT
     ),
 
     [_LOWER] = LAYOUT(
