2c15c30473af5a36fffbaad13d98b10a20e73ad0
Satoru NAKAYA <HKR7Fm8P7U@gmail.com>
2024-08-05 20:42:09 +0900

    自作キーボードcoconut42のキーマップ

---
+++ keyboards/tor_nky/coconut42/keymaps/naginata_v15m/rules.mk
@@ -0,0 +1,17 @@
+#BOOTMAGIC_ENABLE = yes	# Enable Bootmagic Lite
+#MOUSEKEY_ENABLE = yes	# Mouse keys
+#EXTRAKEY_ENABLE = yes	# Audio control and System control
+#NKRO_ENABLE = yes		# Enable N-Key Rollover
+RGBLIGHT_ENABLE = no	# Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE = yes
+#OLED_ENABLE		= yes
+OLED_DRIVER		= ssd1306
+OS_DETECTION_ENABLE = yes
+
+
+RING_BUFFERED_6KRO_REPORT_ENABLE = yes
+	# USB 6-Key Rollover - Instead of stopping any new input once 6 keys
+	# are pressed, the oldest key is released and the new key is pressed.
+DEBOUNCE_TYPE = sym_defer_pk	# debouncing per key.
+
+# CONSOLE_ENABLE = yes		# キースキャンにかかる時間を測定するには、さらに config.h へ #define DEBUG_MATRIX_SCAN_RATE を追加する
