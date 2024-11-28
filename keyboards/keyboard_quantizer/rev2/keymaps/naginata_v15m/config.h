69be7a9afeb07f0b5926be6bc082b09e8f2b8f19
Satoru NAKAYA <HKR7Fm8P7U@gmail.com>
2024-11-28 22:19:35 +0900

    QMK Firmware v0.27対応(一応v0.26用のままでもOK)

--- keyboards/keyboard_quantizer/rev2/keymaps/naginata_v15m/config.h
+++ keyboards/keyboard_quantizer/rev2/keymaps/naginata_v15m/config.h
@@ -4,13 +4,9 @@
 #define TAPPING_TERM 200
 #define PERMISSIVE_HOLD
 
-// fix iPhone and iPad power adapter issue
-// iOS device need lessthan 100
-// #define USB_MAX_POWER_CONSUMPTION 100
 
-
-#define NG_USE_KAWASEMI // Mac汎用 → かわせみ専用
-#define UNICODE_KEY_MAC KC_NO   // Keycodes used for starting Unicode input on different platforms
+// 辞書使用の薙刀式
+#define NG_USE_DIC
 
 // Forces the keyboard to wait for a USB connection to be established before it starts up
 #define WAIT_FOR_USB
