7275d3979e43103993347c65073b5ec057a81590
Satoru NAKAYA <HKR7Fm8P7U@gmail.com>
2024-12-03 19:02:36 +0900

    辞書式は、Macのライブ変換と相性がよくない

--- keyboards/daraku_neko/gpk60_46a/keymaps/naginata_v15m/keymap.c
+++ keyboards/daraku_neko/gpk60_46a/keymaps/naginata_v15m/keymap.c
@@ -146,7 +146,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  static bool is_us2jis = false;
+  static bool is_us2jis = true;
 #ifdef CONSOLE_ENABLE
   const uint16_t key_timer = timer_read();  // 時間測定開始;
 #endif
