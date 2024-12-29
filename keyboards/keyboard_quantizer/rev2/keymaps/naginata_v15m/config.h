e9bb9c15034de79eb5715d42ed363a03f410a1cb
Satoru NAKAYA <HKR7Fm8P7U@gmail.com>
2024-12-29 19:09:38 +0900

    naginata_v15mとnaginata_v15kの統合

--- keyboards/keyboard_quantizer/rev2/keymaps/naginata_v15m/config.h
+++ keyboards/keyboard_quantizer/rev2/keymaps/naginata_v15m/config.h
@@ -8,5 +8,8 @@
 // 辞書使用の薙刀式
 #define NG_USE_DIC
 
+#define NG_USE_KAWASEMI // Mac汎用 → かわせみ専用
+#define UNICODE_KEY_MAC KC_NO   // Keycodes used for starting Unicode input on different platforms
+
 // Forces the keyboard to wait for a USB connection to be established before it starts up
 #define WAIT_FOR_USB
--- users/naginata_v15k/config.h
+++
@@ -1,3 +0,0 @@
-#define UNICODE_TYPE_DELAY 0
-
-#define NG_USE_DIC
