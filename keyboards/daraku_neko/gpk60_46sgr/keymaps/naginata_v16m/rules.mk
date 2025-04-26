92d8ca9b71042a981f035b58c23fcbc6e6e003db
Satoru NAKAYA <HKR7Fm8P7U@gmail.com>
2025-04-26 10:16:29 +0900

    naginata_v16m

---
+++ keyboards/daraku_neko/gpk60_46sgr/keymaps/naginata_v16m/rules.mk
@@ -0,0 +1,10 @@
+#BOOTMAGIC_ENABLE = yes		# Enable Bootmagic Lite
+#MOUSEKEY_ENABLE = yes		# Mouse keys
+#EXTRAKEY_ENABLE = yes		# Audio control and System control
+#NKRO_ENABLE = yes			# Enable N-Key Rollover
+#RGBLIGHT_ENABLE = yes		# Enable keyboard RGB underglow
+
+OS_DETECTION_ENABLE = yes
+DEBOUNCE_TYPE = sym_defer_pk	# debouncing per key.
+
+# CONSOLE_ENABLE = yes
---
+++ keyboards/keyboard_quantizer/rev2/keymaps/naginata_v16m/config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+//#define QUICK_TAP_TERM 0
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+
+
+// 辞書使用の薙刀式
+#define NG_USE_DIC
+
+#define NG_USE_KAWASEMI // Mac汎用 → かわせみ専用
+#define UNICODE_KEY_MAC KC_NO   // Keycodes used for starting Unicode input on different platforms
+
+// Forces the keyboard to wait for a USB connection to be established before it starts up
+#define WAIT_FOR_USB
